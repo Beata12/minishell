474f1d3feb0ef128bdeb9b914e4bf3ed5da663f2
Anjali Nikhra <40138274+anjaliNikhra18@users.noreply.github.com>
2024-05-23 12:05:08 +0200

    Add files via upload

---
+++ minishell_practice/include/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aneekhra <aneekhra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 17:47:43 by aneekhra          #+#    #+#             */
+/*   Updated: 2024/05/22 17:49:11 by aneekhra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// Function prototypes
+void display_prompt();
+
+#endif
---
+++ minishell_practice/src/minishell.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aneekhra <aneekhra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 17:47:33 by aneekhra          #+#    #+#             */
+/*   Updated: 2024/05/22 17:48:52 by aneekhra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void display_prompt() {
+    char *input;
+
+    while (1) {
+        // Display the prompt and read input
+        input = readline("minishell> ");
+
+        // If readline encounters EOF, it returns NULL
+        if (!input) {
+            printf("\n");
+            break;
+        }
+
+        // If the input is not empty, add it to the history
+        if (*input) {
+            add_history(input);
+        }
+
+        // For now, just print the input
+        printf("You entered: %s\n", input);
+
+        // Free the allocated input
+        free(input);
+    }
+}
+
+int main(void) {
+    display_prompt();
+    return 0;
+}
