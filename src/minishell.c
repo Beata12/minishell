046beab90db367d93188b3bb2488699da8269788
Beata Marek <bmarek@c1r7s4.42wolfsburg.de>
2024-06-03 12:39:40 +0200

    rediraction

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: bmarek <bmarek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 17:47:33 by aneekhra          #+#    #+#             */
-/*   Updated: 2024/06/03 10:56:30 by bmarek           ###   ########.fr       */
+/*   Updated: 2024/06/03 11:15:25 by bmarek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,93 +16,93 @@ int g_exit_status;
 // Signal handlers
 void handle_sigint(int sig)
 {
-    g_exit_status = 130;
-    printf("\n");
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	g_exit_status = 130;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 
-    if (sig == SIGTSTP || sig == SIGQUIT)
-    {
-        rl_replace_line("", 0);
-        rl_redisplay();
-    }
+	if (sig == SIGTSTP || sig == SIGQUIT)
+	{
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void handle_sigquit(int sig)
 {
-    (void)sig;
-    // Do nothing on Ctrl-
+	(void)sig;
+	// Do nothing on Ctrl-
 }
 
 void setup_signal_handlers(void)
 {
-    struct sigaction sa;
+	struct sigaction sa;
 
-    sa.sa_handler = handle_sigint;
-    sa.sa_flags = SA_SIGINFO | SA_RESTART;
-    sigemptyset(&sa.sa_mask);
-    rl_catch_signals = 0;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
-    sigaction(SIGTSTP, &sa, NULL);
+	sa.sa_handler = handle_sigint;
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	rl_catch_signals = 0;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+	sigaction(SIGTSTP, &sa, NULL);
 }
 
 void load_history(void)
 {
-    read_history(HISTORY_FILE);
+	read_history(HISTORY_FILE);
 }
 
 void save_history(void)
 {
-    write_history(HISTORY_FILE);
+	write_history(HISTORY_FILE);
 }
 
 char **split_input(char *input)
 {
-    char **args = malloc(64 * sizeof(char*)); // Allocate space for 64 arguments
-    char *arg;
-    int position = 0;
+	char **args = malloc(64 * sizeof(char*)); // Allocate space for 64 arguments
+	char *arg;
+	int position = 0;
 
-    arg = strtok(input, " "); // Split input by spaces
-    while (arg != NULL)
-    {
-        args[position++] = arg;
-        arg = strtok(NULL, " ");
-    }
-    args[position] = NULL; // Null-terminate the array of arguments
-    return args;
+	arg = strtok(input, " "); // Split input by spaces
+	while (arg != NULL)
+	{
+		args[position++] = arg;
+		arg = strtok(NULL, " ");
+	}
+	args[position] = NULL; // Null-terminate the array of arguments
+	return args;
 }
 
 // Function to find the full path of an executable
 char *find_executable(char *command)
 {
-    char *path = getenv("PATH");
-    char *token;
-    char full_path[1024];
+	char *path = getenv("PATH");
+	char *token;
+	char full_path[1024];
 
-    // If the command is an absolute or relative path, return it directly
-    if (strchr(command, '/') != NULL)
-        return command;
+	// If the command is an absolute or relative path, return it directly
+	if (strchr(command, '/') != NULL)
+		return command;
 
-    // Search for the command in the PATH directories
-    token = strtok(path, ":");
-    while (token != NULL)
-    {
-        full_path[0] = '\0';
-        strcat(full_path, token);
-        strcat(full_path, "/");
-        strcat(full_path, command);
+	// Search for the command in the PATH directories
+	token = strtok(path, ":");
+	while (token != NULL)
+	{
+		full_path[0] = '\0';
+		strcat(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, command);
 
-        if (access(full_path, X_OK) == 0)
-        {
-            char *executable = malloc(strlen(full_path) + 1);
-            strcpy(executable, full_path);
-            return executable;
-        }
-        token = strtok(NULL, ":");
-    }
-    return NULL;
+		if (access(full_path, X_OK) == 0)
+		{
+			char *executable = malloc(strlen(full_path) + 1);
+			strcpy(executable, full_path);
+			return executable;
+		}
+		token = strtok(NULL, ":");
+	}
+	return NULL;
 }
 int ft_newline(char **av)
 {
@@ -135,89 +135,87 @@ int ft_execvp(const char *args, char **argv)
 // Function to execute a command
 void execute_command(char **args)
 {
-    char *command = args[0];
-    char *executable = find_executable(command);
+	char *command = args[0];
+	char *executable = find_executable(command);
 
-    if (executable == NULL)
-    {
-        printf("minishell: command not found: %s\n", command);
-        return;
-    }
+	if (executable == NULL)
+	{
+		printf("minishell: command not found: %s\n", command);
+		return;
+	}
 
-    pid_t pid = fork();
-    if (pid == 0)
-    {
-        // Child process
-        if (ft_execvp(args[0], args) == -1)
-        {
-            perror("minishell");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0)
-    {
-        // Error forking
-        perror("minishell");
-    }
-    else
-    {
-        // Parent process
-        int status;
-        waitpid(pid, &status, 0);
-    }
-    // Free the allocated memory for the executable path if it was duplicated
-    free(executable);
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+		// Child process
+		if (ft_execvp(args[0], args) == -1)
+		{
+			perror("minishell");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0)
+	{
+		// Error forking
+		perror("minishell");
+	}
+	else
+	{
+		// Parent process
+		int status;
+		waitpid(pid, &status, 0);
+	}
+	// Free the allocated memory for the executable path if it was duplicated
+	free(executable);
 }
 
 void error_str(void)
 {
-    g_exit_status = 258;
+	g_exit_status = 258;
 }
 
 volatile sig_atomic_t sigint_received = 0;
 
 // Signal handler function for SIGINT (Ctrl+C)
 void sigint_handler(int signum) {
-    sigint_received = 1;
+	sigint_received = 1;
 }
 
 void display_prompt(void)
 {
-    char *input;
+	char *input;
 
-    load_history();
+	load_history();
 
-    // Register SIGINT handler
-    signal(SIGINT, sigint_handler);
+	// Register SIGINT handler
+	signal(SIGINT, sigint_handler);
 	
-//     while (1) {
-    while (!sigint_received) {
-        input = readline("minishell> ");
-        if (!input) {
-            printf("exit\n");
-            break;
-        }
-        if (*input) {
-            add_history(input);
-        }
-        if (check_str(input) != 0)
-        {
-            error_str();
-        }   
-        else
-        {
-            // handle_redirection(input);
-            parser(input);
-        }
-        free(input);
-    }
-    save_history();
+//	 while (1) {
+	while (!sigint_received)
+	{
+		input = readline("minishell> ");
+		if (!input)
+		{
+			printf("exit\n");
+			break;
+		}
+		if (*input)
+			add_history(input);
+		if (check_str(input) != 0)
+			error_str();
+		else
+			// handle_redirection(input);
+			parser(input);
+		free(input);
+	}
+	save_history();
 }
 
 int main(int argc, char **argv)
-{	if (!argc && !argv)
+{
+	if (!argc && !argv)
 		return (0);
-    g_exit_status = 0;
-    setup_signal_handlers();
-    display_prompt();
-    return g_exit_status;
+	g_exit_status = 0;
+	setup_signal_handlers();
+	display_prompt();
+	return g_exit_status;
 }
--- src/rediraction.c
+++ src/rediraction.c
@@ -6,66 +6,124 @@
 /*   By: bmarek <bmarek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 10:01:18 by bmarek            #+#    #+#             */
-/*   Updated: 2024/06/03 10:08:01 by bmarek           ###   ########.fr       */
+/*   Updated: 2024/06/03 12:37:34 by bmarek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void redirect_input(const char *filename) {
+void	handle_heredoc(const char *delimiter)
+{
+	char	*line = NULL;
+	size_t	len = 0;
+	FILE	*tmp_file = tmpfile();
+
+	if (!tmp_file) {
+		perror("tmpfile");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		printf("> ");
+		getline(&line, &len, stdin);
+		if (strncmp(line, delimiter, strlen(delimiter)) == 0
+			&& line[strlen(delimiter)] == '\n')
+		{
+			break;
+		}
+		fprintf(tmp_file, "%s", line);
+	}
+	fseek(tmp_file, 0, SEEK_SET);
+	if (dup2(fileno(tmp_file), STDIN_FILENO) == -1)
+	{
+		perror("dup2");
+		exit(EXIT_FAILURE);
+	}
+	free(line);
+	fclose(tmp_file);
+}
+
+// void	handle_redirection(char *input)
+// {
+// 	char	*token;
+// 	char	*filename;
+// 	int		append = 0;
+
+// 	while ((token = strpbrk(input, "<>")))
+// 	{
+// 		if (*token == '<')
+// 		{
+// 			if (*(token + 1) == '<')
+// 			{// handle heredoc
+// 				*token = '\0';
+// 				filename = strtok(token + 2, " ");
+// 				handle_heredoc(filename);
+// 			}
+// 			else
+// 			{// handle input redirection
+// 				*token = '\0';
+// 				filename = strtok(token + 1, " ");
+// 				redirect_input(filename);
+// 			}
+// 		}
+// 		else if (*token == '>')
+// 		{
+// 			if (*(token + 1) == '>')
+// 			{// handle output append redirection
+// 				append = 1;
+// 				*token = '\0';
+// 				filename = strtok(token + 2, " ");
+// 			}
+// 			else
+// 			{// handle output redirection
+// 				append = 0;
+// 				*token = '\0';
+// 				filename = strtok(token + 1, " ");
+// 			}
+// 			redirect_output(filename, append);
+// 		}
+// 		input = token + strlen(filename) + 1;
+// 	}
+// }
+
+int redirect_input(const char *filename)
+{       
     int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
+    
+    if (fd == -1)
+    {
         perror("open");
-        exit(EXIT_FAILURE);
+        return -1; // Zwracamy -1 w przypadku błędu
     }
-    if (dup2(fd, STDIN_FILENO) == -1) {
+    if (dup2(fd, STDIN_FILENO) == -1)
+    {
         perror("dup2");
-        exit(EXIT_FAILURE);
+        close(fd);
+        return -1; // Zwracamy -1 w przypadku błędu
     }
     close(fd);
+    return 0; // Zwracamy 0 w przypadku sukcesu
 }
 
-void redirect_output(const char *filename, int append) {
+int redirect_output(const char *filename, int append)
+{
     int fd = open(filename, O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC), 0644);
-    if (fd == -1) {
+    
+    if (fd == -1)
+    {
         perror("open");
-        exit(EXIT_FAILURE);
+        return -1; // Zwracamy -1 w przypadku błędu
     }
-    if (dup2(fd, STDOUT_FILENO) == -1) {
+    if (dup2(fd, STDOUT_FILENO) == -1)
+    {
         perror("dup2");
-        exit(EXIT_FAILURE);
+        close(fd);
+        return -1; // Zwracamy -1 w przypadku błędu
     }
     close(fd);
+    return 0; // Zwracamy 0 w przypadku sukcesu
 }
 
-void handle_heredoc(const char *delimiter) {
-    char *line = NULL;
-    size_t len = 0;
-    FILE *tmp_file = tmpfile(); // Zmieniona nazwa zmiennej
-
-    if (!tmp_file) {
-        perror("tmpfile");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1) {
-        printf("> ");
-        getline(&line, &len, stdin);
-        if (strncmp(line, delimiter, strlen(delimiter)) == 0
-			&& line[strlen(delimiter)] == '\n') {
-            break;
-        }
-        fprintf(tmp_file, "%s", line);
-    }
-
-    fseek(tmp_file, 0, SEEK_SET);
-    if (dup2(fileno(tmp_file), STDIN_FILENO) == -1) {
-        perror("dup2");
-        exit(EXIT_FAILURE);
-    }
-    free(line);
-    fclose(tmp_file);
-}
 
 void handle_redirection(char *input)
 {
@@ -73,28 +131,44 @@ void handle_redirection(char *input)
     char *filename;
     int append = 0;
 
-    while ((token = strpbrk(input, "<>"))) {
-        if (*token == '<') {
-            if (*(token + 1) == '<') { // handle heredoc
+    while ((token = strpbrk(input, "<>")))
+    {
+        if (*token == '<')
+        {
+            if (*(token + 1) == '<')
+            { // handle heredoc
                 *token = '\0';
                 filename = strtok(token + 2, " ");
                 handle_heredoc(filename);
-            } else { // handle input redirection
+            }
+            else
+            { // handle input redirection
                 *token = '\0';
                 filename = strtok(token + 1, " ");
-                redirect_input(filename);
+                if (redirect_input(filename) == -1) {
+                    perror("redirect_input");
+                    return; // Kontynuujemy działanie pomimo błędu
+                }
             }
-        } else if (*token == '>') {
-            if (*(token + 1) == '>') { // handle output append redirection
+        }
+        else if (*token == '>')
+        {
+            if (*(token + 1) == '>')
+            { // handle output append redirection
                 append = 1;
                 *token = '\0';
                 filename = strtok(token + 2, " ");
-            } else { // handle output redirection
+            }
+            else
+            { // handle output redirection
                 append = 0;
                 *token = '\0';
                 filename = strtok(token + 1, " ");
             }
-            redirect_output(filename, append);
+            if (redirect_output(filename, append) == -1) {
+                perror("redirect_output");
+                return; // Kontynuujemy działanie pomimo błędu
+            }
         }
         input = token + strlen(filename) + 1;
     }
