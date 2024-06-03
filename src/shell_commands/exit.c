36455e6b1eb5804da62786fd564f2489e55a4439
Beata Marek <bmarek@c1r7s4.42wolfsburg.de>
2024-06-03 22:17:28 +0200

    h

--- src/shell_commands/exit.c
+++ src/shell_commands/exit.c
@@ -6,21 +6,11 @@
 /*   By: bmarek <bmarek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 09:58:42 by bmarek            #+#    #+#             */
-/*   Updated: 2024/06/03 20:55:30 by bmarek           ###   ########.fr       */
+/*   Updated: 2024/06/03 22:17:20 by bmarek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/parsing.h"
 
-int shell_exit(Token *args)
-{
-    // Free memory allocated for each argument
-    if (args != NULL) {
-        for (int i = 0; args[i].value2 != NULL; i++) {
-            free(args[i].value2);
-        }
-        //free(args);
-    }
-    exit(0);
-}
+
